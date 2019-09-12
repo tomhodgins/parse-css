3e8aece64547295fa1ec25db2f425d2e2a5cd23d
Tommy Hodgins <tomhodgins@gmail.com>
2019-09-12 01:01:10 -0400

    Add cjs-formatted version and some demos

---
+++ test/node-demo.js
@@ -0,0 +1,10 @@
+// parse-css deno demo
+const parseCSS = require('../index.cjs.js')
+
+console.log(
+  JSON.stringify(
+    parseCSS.parseAStylesheet('div { color: lime }'),
+    null,
+    2
+  )
+)
